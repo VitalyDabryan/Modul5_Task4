1a99a49ce4efe26c19df3660e14030c166b70935
VitalyDabryan <vital.1977@tut.by>
2022-07-03 19:39:27 +0300

    3commit

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,5 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("There's nothing to see here");
         System.out.println("2-nd commit");
+        System.out.println("3-nd commit");
     }
 }
